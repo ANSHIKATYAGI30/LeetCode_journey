b3a88925be37dd062a6ff2220f3a5bec8098804a
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-05-12 12:03:53 +0530

    Create palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+
+    // Negative numbers cannot be palindrome
+    if(x < 0) {
+        return false;
+    }
+
+    let original = x;
+    let reverse = 0;
+
+    while(x > 0) {
+
+        let digit = x % 10;
+
+        reverse = reverse * 10 + digit;
+
+        x = Math.floor(x / 10);
+    }
+
+    return original === reverse;
+};

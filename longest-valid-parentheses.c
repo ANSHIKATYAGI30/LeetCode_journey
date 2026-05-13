4cc288df74ff2740a3b21712665a2500324a34f7
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-05-13 14:13:44 +0530

    Create longest-valid-parentheses.c
    1. If '(' → push index
    2. If ')' → pop stack
    3. If stack becomes empty → push current index as new base
    4. Else → update max length

---
+++ longest-valid-parentheses.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+
+int longestValidParentheses(char* s) {
+
+    int n = 0;
+
+    while(s[n] != '\0') {
+        n++;
+    }
+
+    int* stack = (int*)malloc((n + 1) * sizeof(int));
+
+    int top = -1;
+
+    // Base index
+    stack[++top] = -1;
+
+    int maxLen = 0;
+
+    for(int i = 0; i < n; i++) {
+
+        if(s[i] == '(') {
+
+            stack[++top] = i;
+        }
+        else {
+
+            top--;
+
+            // Invalid base
+            if(top == -1) {
+
+                stack[++top] = i;
+            }
+            else {
+
+                int len = i - stack[top];
+
+                if(len > maxLen) {
+
+                    maxLen = len;
+                }
+            }
+        }
+    }
+
+    free(stack);
+
+    return maxLen;
+}

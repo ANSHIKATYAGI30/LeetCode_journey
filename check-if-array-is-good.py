d7d58e01c80df652d435aa91a19d9af305c57a64
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-05-22 14:19:34 +0530

    Create check-if-array-is-good.py

---
+++ check-if-array-is-good.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isGood(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        n = max(nums)
+
+        # Length must be n + 1
+        if len(nums) != n + 1:
+            return False
+
+        nums.sort()
+
+        # Check 1 to n-1 appear once
+        for i in range(1, n):
+            if nums[i - 1] != i:
+                return False
+
+        # Last two elements must be n
+        return nums[-1] == n and nums[-2] == n

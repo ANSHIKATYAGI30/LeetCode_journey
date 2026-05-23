237daa920829b1f8f6c0109c107ebf7a5a769a37
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-05-23 14:04:30 +0530

    Create intersection-of-LL.c

---
+++ intersection-of-LL.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+
+    struct ListNode *a = headA;
+    struct ListNode *b = headB;
+
+    while (a != b) {
+
+        // When reaching end, jump to other list
+        a = (a == NULL) ? headB : a->next;
+        b = (b == NULL) ? headA : b->next;
+    }
+
+    return a; // intersection node or NULL
+}

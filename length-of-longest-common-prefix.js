8dfcf661c419747f69a9330ca69dab756961c042
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-05-23 13:19:47 +0530

    Create length-of-longest-common-prefix.js

---
+++ length-of-longest-common-prefix.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number}
+ */
+var longestCommonPrefix = function(arr1, arr2) {
+
+    let prefixes = new Set();
+
+    // Store all prefixes from arr1
+    for (let num of arr1) {
+
+        let str = num.toString();
+
+        for (let i = 1; i <= str.length; i++) {
+            prefixes.add(str.slice(0, i));
+        }
+    }
+
+    let ans = 0;
+
+    // Check prefixes in arr2
+    for (let num of arr2) {
+
+        let str = num.toString();
+
+        for (let i = 1; i <= str.length; i++) {
+
+            let prefix = str.slice(0, i);
+
+            if (prefixes.has(prefix)) {
+                ans = Math.max(ans, i);
+            }
+        }
+    }
+
+    return ans;
+};

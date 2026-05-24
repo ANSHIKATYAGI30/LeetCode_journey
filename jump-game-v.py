aff5366ed2934a4baad19fb98ce22eb9a59af433
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-05-24 15:46:16 +0530

    Create jump-game-v.py

---
+++ jump-game-v.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def maxJumps(self, arr, d):
+        """
+        :type arr: List[int]
+        :type d: int
+        :rtype: int
+        """
+
+        n = len(arr)
+        dp = [-1] * n
+
+        def dfs(i):
+
+            if dp[i] != -1:
+                return dp[i]
+
+            ans = 1
+
+            # Jump right
+            for j in range(i + 1, min(n, i + d + 1)):
+
+                if arr[j] >= arr[i]:
+                    break
+
+                ans = max(ans, 1 + dfs(j))
+
+            # Jump left
+            for j in range(i - 1, max(-1, i - d - 1), -1):
+
+                if arr[j] >= arr[i]:
+                    break
+
+                ans = max(ans, 1 + dfs(j))
+
+            dp[i] = ans
+            return ans
+
+        return max(dfs(i) for i in range(n))

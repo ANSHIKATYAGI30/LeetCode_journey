c517dc18eb0b449c76c7142199b45f4bb89e5d6c
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-05-25 14:43:56 +0530

    Create implement-queue-using-stacks.py

---
+++ implement-queue-using-stacks.py
@@ -0,0 +1,46 @@
+class MyQueue(object):
+
+    def __init__(self):
+
+        self.s1 = []
+        self.s2 = []
+
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+
+        self.s1.append(x)
+
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+
+        self.peek()
+        return self.s2.pop()
+
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+
+        # Move elements only when s2 is empty
+        if not self.s2:
+
+            while self.s1:
+                self.s2.append(self.s1.pop())
+
+        return self.s2[-1]
+
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+
+        return not self.s1 and not self.s2

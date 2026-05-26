3c53d366f8e320fa32a4bfa8d8f67bbd63403def
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-05-26 11:57:01 +0530

    Create convert_to_hexadecimal.py

---
+++ convert_to_hexadecimal.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def toHex(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if num == 0:
+          return "0"
+
+        # Handle negative numbers using 32-bit two's complement
+        num &= 0xffffffff
+
+        hex_chars = "0123456789abcdef"
+
+        result = ""
+
+        while num > 0:
+
+            digit = num % 16
+
+            result = hex_chars[digit] + result
+
+            num //= 16
+
+        return result

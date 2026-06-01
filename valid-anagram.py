861fb75168078fff65eb2780e8f4eb41f563660b
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-06-01 12:49:31 +0530

    Create valid-anagram.py

---
+++ valid-anagram.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        # Count characters in s
+        for ch in s:
+            count[ch] = count.get(ch, 0) + 1
+
+        # Remove using t
+        for ch in t:
+            if ch not in count:
+                return False
+
+            count[ch] -= 1
+
+            if count[ch] < 0:
+                return False
+
+        return True

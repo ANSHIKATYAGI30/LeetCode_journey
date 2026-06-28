dbe747cca5c265264ff794c22216078416758d93
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-06-29 00:41:56 +0530

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -4,21 +4,15 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-
         result = []
 
         for i in range(1, n + 1):
-
             if i % 3 == 0 and i % 5 == 0:
                 result.append("FizzBuzz")
-
             elif i % 3 == 0:
                 result.append("Fizz")
-
             elif i % 5 == 0:
                 result.append("Buzz")
-
             else:
                 result.append(str(i))
-
         return result

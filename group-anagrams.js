4f47aa34ea8f944c953f83dd6ec7ef9f455550a7
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-07-29 22:16:16 +0530

    Update group-anagrams.js

--- group-anagrams.js
+++ group-anagrams.js
@@ -7,11 +7,9 @@ var groupAnagrams = function(strs) {
     
     for(let str of strs){
         let key = str.split('').sort().join('');
-
         if(!map.has(key)){
             map.set(key,[]);
         }
-
         map.get(key).push(str);
     }
     return Array.from(map.values());
